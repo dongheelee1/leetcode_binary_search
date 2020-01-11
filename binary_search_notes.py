686c3164dc84cf9de126c33c055c9f4aae1826c4
donghizzle <dongheec.lee@gmail.com>
2020-01-11 14:31:29 -0500

    Create binary_search_notes.py

---
+++ binary_search_notes.py
@@ -0,0 +1,4 @@
+Binary search is an algorithm that looks for a target value in an ORDERED array. 
+After every loop/recursion, the algorithm cuts down the search space in half.  
+
+Binary search won't work if the array is not ordered, so one must sort the array first. 
